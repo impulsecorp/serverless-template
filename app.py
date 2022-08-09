e367daac0e30e571e8e13e9d90d6e99b4e116c7a
Jayr <jayrcastro.py@gmail.com>
2022-08-09 19:34:21 +0800

    added timm model in app py

--- app.py
+++ app.py
@@ -1,26 +1,43 @@
 from transformers import pipeline
+import cv2
+# from utils import *
+from timm.models import create_model
+from timm.data import resolve_data_config
+import torch.nn as nn
 import torch
 
 # Init is ran on server startup
 # Load your model to GPU as a global variable here using the variable name "model"
 def init():
     global model
-    
-    device = 0 if torch.cuda.is_available() else -1
-    model = pipeline('fill-mask', model='bert-base-uncased', device=device)
+    global config
+
+    model = create_model(
+                'efficientnet_b0',
+                num_classes=2,
+                in_chans=3,
+                pretrained=True,
+                checkpoint_path='best_models/efficientnet_b0/model_best.pth.tar' # maybe download the model from colab
+                    )
+    model = model.cuda()
+    model.eval()
 
 # Inference is ran for every server call
 # Reference your preloaded global model variable here.
 def inference(model_inputs:dict) -> dict:
     global model
+    global config
+    # here we need to download the image and save it somewhere 
+    # so we can read it via torch and insert it in cuda
 
-    # Parse out your arguments
-    prompt = model_inputs.get('prompt', None)
-    if prompt == None:
-        return {'message': "No prompt provided"}
-    
-    # Run the model
-    result = model(prompt)
+    img = cv2.imread('01a51d500097.png')
+    with torch.no_grad():
+        
+        img = torch.from_numpy(img).float().to('cuda')
+        img = img.permute(2, 0, 1)
+        img.unsqueeze_(0)
+        labels = model(img)
+        # topk = labels.topk(k)[1]
+        label = labels.topk(1)[1].item()
 
-    # Return the results as a dictionary
-    return result
+    return {'result':label}
